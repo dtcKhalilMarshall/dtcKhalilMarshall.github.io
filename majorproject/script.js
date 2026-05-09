f0b865deb261af54b5c67e41361d7f86db2ab1f8
dtcKhalilMarshall <khalil.marshall@wsu.edu>
2026-05-08 22:00:51 -0700

    Enhance video player and add artist search feature
    Refactored video loading logic and added artist search functionality.

--- majorproject/script.js
+++ majorproject/script.js
@@ -2,75 +2,109 @@ const tracks = [
   { title: "Steve Lacy - Dark Red", url: "https://www.youtube.com/embed/Q6FarZpy67M?si=83k3oUx6dYD5AtQk" },
   { title: "Steve Lacy - C U Girl", url: "https://www.youtube.com/embed/t0fn0Q0DB0E?si=LbWg4r_BJVdMfYI_" },
   { title: "Steve Lacy - Ryd", url: "https://www.youtube.com/embed/BCV_vlRDrsQ?si=LstQmGucR-P0hN2E" },
-  { title: "Beyoncé - Halo", url: "https://www.youtube.com/embed/bnVUHWCynig" },
-  { title: "Beyoncé - Crazy In Love", url: "https://www.youtube.com/embed/ViwtNLUqkMY" },
-  { title: "Beyoncé - Irreplacable", url: "https://www.youtube.com/embed/3iTnJX7aU5E" },
-  { title: "Kendrick Lamar - HUMBLE.", url: "https://www.youtube.com/embed/tvTRZJ-4EyI" },
-  { title: "Kendrick Lamar - All The Stars", url: "https://www.youtube.com/embed/4iGIl8V5C9k" },
-  { title: "Kendrick Lamar - DNA.", url: "https://www.youtube.com/embed/ktvTqknDobU" },
-  { title: "Frank Ocean - Chanel", url: "https://www.youtube.com/embed/7E2fFzU7g8I" },
-  { title: "Frank Ocean - Lost", url: "https://www.youtube.com/embed/Vp7gqEgQb5E" },
-  { title: "Frank Ocean - Thinkin Bout You", url: "https://www.youtube.com/embed/9f6e7Ojh0zQ" },
-  { title: "PinkPantheress - Just for me", url: "https://www.youtube.com/embed/0p8lLsF7Hes" },
-  { title: "PinkPantheress - Break It Off", url: "https://www.youtube.com/embed/U7u4U2VY7dQ" },
-  { title: "PinkPantheress - Passion", url: "https://www.youtube.com/embed/jidV2N6DH1c" },
-  { title: "Miguel - Adorn", url: "https://www.youtube.com/embed/3MFp9E3W7SI" },
-  { title: "Miguel - Sure Thing", url: "https://www.youtube.com/embed/8rCzM0hE7wE" },
-  { title: "Miguel - Waves", url: "https://www.youtube.com/embed/0f4QYEGzIEk" },
-  { title: "Megan Thee Stallion - Savage", url: "https://www.youtube.com/embed/8dWLZy3P27o" },
-  { title: "Megan Thee Stallion - Body", url: "https://www.youtube.com/embed/SGk5_K7X9E0" },
-  { title: "Megan Thee Stallion - Thot Shit", url: "https://www.youtube.com/embed/7M0LJQwGd8k" },
-  { title: "Kehlani - Gangsta", url: "https://www.youtube.com/embed/7aNPSsUQKk0" },
-  { title: "Kehlani - Honey", url: "https://www.youtube.com/embed/7-5WvXxQm8k" },
-  { title: "Kehlani - Nights Like This", url: "https://www.youtube.com/embed/1K4V5XoC4o4" },
-  { title: "Doechii - Persuasive", url: "https://www.youtube.com/embed/4Ff3K6lY6pk" },
-  { title: "Doechii - Crazy", url: "https://www.youtube.com/embed/xU1h3fF7H9U" },
-  { title: "Doechii - Yucky Blucky Fruitcake", url: "https://www.youtube.com/embed/8h7F8sL1VYk" },
-  { title: "Olivia Dean - Dive", url: "https://www.youtube.com/embed/0V0B8U9O7Kk" },
-  { title: "Olivia Dean - The Hardest Part", url: "https://www.youtube.com/embed/0U78V7W7l8E" },
-  { title: "Olivia Dean - Echo", url: "https://www.youtube.com/embed/1Vx3FtHkKH0" },
-  { title: "Ravyn Lenae - Sticky", url: "https://www.youtube.com/embed/0R9b7XKX3iY" },
-  { title: "Ravyn Lenae - Something To Feel", url: "https://www.youtube.com/embed/s8V1zU7mQos" },
-  { title: "Ravyn Lenae - Love Like This", url: "https://www.youtube.com/embed/5Dq3u8p1iH4" },
-  { title: "Rochelle Jordan - Come Back", url: "https://www.youtube.com/embed/7f7p7QMi8hI" },
-  { title: "Rochelle Jordan - DAYONE", url: "https://www.youtube.com/embed/4XhA2NwBk4g" },
-  { title: "Rochelle Jordan - No Time", url: "https://www.youtube.com/embed/7rG2A8sH8mY" },
-  { title: "Solange - Cranes in the Sky", url: "https://www.youtube.com/embed/4D1xycK5dP4" },
-  { title: "Solange - Losing You", url: "https://www.youtube.com/embed/7zUe1d8UfE4" },
-  { title: "Solange - Almeda", url: "https://www.youtube.com/embed/4yF5U7zXfWk" },
-  { title: "Tinashe - 2 On", url: "https://www.youtube.com/embed/5hG9j2Rz2uY" },
-  { title: "Tinashe - No Drama", url: "https://www.youtube.com/embed/3U7BMI0z5U0" },
-  { title: "Tinashe - Flame", url: "https://www.youtube.com/embed/7sU_6gS6o40" },
-  { title: "Tems - Free Mind", url: "https://www.youtube.com/embed/0Xy7hZp7iPg" },
-  { title: "Tems - Crazy Tings", url: "https://www.youtube.com/embed/mg7z6r3Y3OQ" },
-  { title: "Tems - Damages", url: "https://www.youtube.com/embed/l4V6S6b5VhE" },
-  { title: "Victoria Monet - Moment", url: "https://www.youtube.com/embed/Zp0j0-R4x_k" },
-  { title: "Victoria Monet - Feelings", url: "https://www.youtube.com/embed/6a2z-pzA6A4" },
-  { title: "Victoria Monet - Experience", url: "https://www.youtube.com/embed/7KzWJH9X0xQ" },
-  { title: "Flo - Cardboard Box", url: "https://www.youtube.com/embed/6F7A7gz2_W8" },
-  { title: "Flo - Not My Job", url: "https://www.youtube.com/embed/4WfVnVj6I4g" },
-  { title: "Flo - Immature", url: "https://www.youtube.com/embed/cDEYI4sH4W4" }
 ];
 
- let currentTrackIndex = 0;
+let currentTrackIndex = 0;
 
-  function loadVideo(index) {
-    const video = tracks[index];
-    const container = document.getElementById('youtube-player');
-    container.innerHTML = `
-     <iframe width="560" height="315" src="https://www.youtube.com/embed/BCV_vlRDrsQ?si=67necbl5WKSiGB1k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    `;
-    document.getElementById('track-title').textContent = `Now Playing: ${video.title}`;
-  }
+function loadVideo(index) {
+  const video = tracks[index];
+  const container = document.getElementById('youtube-player');
 
-  // Load first video
+  // Clear previous iframe
+  container.innerHTML = '';
+
+  // Create new iframe
+  const iframe = document.createElement('iframe');
+  iframe.width = '560';
+  iframe.height = '315';
+  iframe.src = video.url;
+  iframe.title = 'YouTube video player';
+  iframe.frameBorder = '0';
+  iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+  iframe.allowFullscreen = true;
+  iframe.referrerPolicy = 'strict-origin-when-cross-origin';
+
+  container.appendChild(iframe);
+
+  // Update track title
+  document.getElementById('track-title').textContent = `Now Playing: ${video.title}`;
+}
+
+// Load initial video
+loadVideo(currentTrackIndex);
+
+// Event listeners for buttons
+document.getElementById('nextVideo').addEventListener('click', () => {
+  currentTrackIndex = (currentTrackIndex + 1) % tracks.length;
   loadVideo(currentTrackIndex);
+});
 
-  document.getElementById('nextVideo').addEventListener('click', () => {
-    currentTrackIndex = (currentTrackIndex + 1) % tracks.length;
-    loadVideo(currentTrackIndex);
-  });
-  document.getElementById('prevVideo').addEventListener('click', () => {
-    currentTrackIndex = (currentTrackIndex - 1 + tracks.length) % tracks.length;
-    loadVideo(currentTrackIndex);
-  });
+document.getElementById('prevVideo').addEventListener('click', () => {
+  currentTrackIndex = (currentTrackIndex - 1 + tracks.length) % tracks.length;
+  loadVideo(currentTrackIndex);
+});
+
+// List of artists with their names and Spotify links
+const artists = [
+  { name: "Steve Lacy", spotify: "https://open.spotify.com/artist/57vWImR43h4CaDao012Ofp" },
+  { name: "Rihanna", spotify: "https://open.spotify.com/artist/5pKCCKE2ajJHZ9KAiaK11H" },
+  { name: "Melody Bloom", spotify: "https://open.spotify.com/search/melodybloom" },
+  { name: "Chenayder", spotify: "https://open.spotify.com/artist/0BXnfQpsxFevtKePq6eX4y" },
+  { name: "Laila!", spotify: "https://open.spotify.com/artist/7tHTnyrdTNM152slc8aQ3t" },
+  { name: "Willow Smith", spotify: "https://open.spotify.com/artist/3rWZHrfrsPBxVy692yAIxF" },
+  { name: "Essosa", spotify: "https://open.spotify.com/artist/6fn7JM8daBvCxpTkYyqtGY" },
+  { name: "Sade", spotify: "https://open.spotify.com/artist/47zz7sob9NUcODy0BTDvKx" },
+  { name: "MOIO", spotify: "https://open.spotify.com/artist/4D35LiRbeEHboAHa0bYUJb" },
+  { name: "keltiey", spotify: "https://open.spotify.com/artist/6ffRXY5wKedZhPTMa6WGys" },
+  { name: "Lil Hero", spotify: "https://open.spotify.com/artist/2imvddeWiv58tUPKh8q3kO" },
+  { name: "Aya Nakamura", spotify: "https://open.spotify.com/artist/7IlRNXHjoOCgEAWN5qYksg" },
+  { name: "Luci4", spotify: "https://open.spotify.com/artist/1CbA4z6JauNQnHzOErDQL6" },
+  { name: "Kanii", spotify: "https://open.spotify.com/artist/1S82w4yw9TYIHZ889mPPaW" },
+  { name: "Frank Ocean", spotify: "https://open.spotify.com/artist/2h93pZq0e7k5yf4dywlkpM" },
+  { name: "Pink Pantheress", spotify: "https://open.spotify.com/artist/78rUTD7y6Cy67W1RVzYs7t" },
+  { name: "Miguel", spotify: "https://open.spotify.com/artist/360IAlyVv4PCEVjgyMZrxK" },
+  { name: "Megan Thee Stallion", spotify: "https://open.spotify.com/artist/181bsRPaVXVlUKXrxwZfHK" },
+  { name: "Kehlani", spotify: "https://open.spotify.com/artist/0cGUm45nv7Z6M6qdXYQGTX" },
+  { name: "Cookiee Kawaii", spotify: "https://open.spotify.com/artist/0DbBBj0ScPumRqKXswGQH1" },
+  { name: "Tyler The Creator", spotify: "https://open.spotify.com/artist/4V8LLVI7PbaPR0K2TGSxFF" },
+  { name: "SZA", spotify: "https://open.spotify.com/artist/7tYKF4w9nC0nq9CsPZTHyP" },
+  { name: "A$AP Rocky", spotify: "https://open.spotify.com/artist/13ubrt8QOOCPljQ2FL1Kca" },
+  { name: "Kendrick Lamar", spotify: "https://open.spotify.com/artist/2YZyLoL8N0Wb9xBt1NhZWg" },
+  { name: "Drake", spotify: "https://open.spotify.com/artist/3TVXtAsR1Inumwj472S9r4" },
+  { name: "Childish Gambino", spotify: "https://open.spotify.com/artist/73sIBHcqh3Z3NyqHKZ7FOL" },
+  { name: "Tyla", spotify: "https://open.spotify.com/artist/3SozjO3Lat463tQICI9LcE" },
+  { name: "Doja Cat", spotify: "https://open.spotify.com/artist/5cj0lLjcoR7YOSnhnX0Po5" },
+  { name: "Leon Thomas", spotify: "https://open.spotify.com/artist/0nnBZ8FXWjG9wZgM2cpfeb" },
+  { name: "Beyoncé", spotify: "https://open.spotify.com/artist/6vWDO969PvNqNYHIOW5v0m" },
+  { name: "Doechii", spotify: "https://open.spotify.com/artist/4E2rKHVDssGJm2SCDOMMJB" },
+  { name: "Olivia Dean", spotify: "https://open.spotify.com/artist/00x1fYSGhdqScXBRpSj3DW" },
+  { name: "Ravyn Lenae", spotify: "https://open.spotify.com/artist/5RTLRtXjbXI2lSXc6jxlAz" },
+  { name: "Rochelle Jordan", spotify: "https://open.spotify.com/artist/3MM3uKNdJbvefUael12dl3" },
+  { name: "Solange", spotify: "https://open.spotify.com/artist/2auiVi8sUZo17dLy1HwrTU" },
+  { name: "Tinashe", spotify: "https://open.spotify.com/artist/0NIIxcxNHmOoyBx03SfTCD" },
+  { name: "Tems", spotify: "https://open.spotify.com/artist/687cZJR45JO7jhk1LHIbgq" },
+  { name: "Victoria Monet", spotify: "https://open.spotify.com/artist/63XBtGSEZINSyXylZxEUbv" },
+  { name: "Flo", spotify: "https://open.spotify.com/artist/0s4kXsjYeH0S1xRyVGN4NO" },
+  { name: "Say Now", spotify: "https://open.spotify.com/artist/1p6HheoJqfYMiMC62yQIWn" },
+  { name: "3Quency", spotify: "https://open.spotify.com/artist/0Im6nZsuvL1W8yJg39xROA" },
+  { name: "SZN4", spotify: "https://open.spotify.com/artist/3aVcen0IJtz6svz8DVlpfc" },
+  { name: "Soulidified", spotify: "https://open.spotify.com/artist/3nE9FhhwfDdyuObjky2v55" },
+  { name: "Bryson Tiller", spotify: "https://open.spotify.com/artist/2EMAnMvWE2eb56ToJVfCWs" },
+  { name: "Bryn Faiyaz", spotify: "https://open.spotify.com/artist/3tlXnStJ1fFhdScmQeLpuG" },
+  { name: "PARTYNEXTDOOR", spotify: "https://open.spotify.com/artist/2HPaUgqeutzr3jx5a9WyDV" },
+  { name: "Don Toliver", spotify: "https://open.spotify.com/artist/4Gso3d4CscCijv0lmajZWs" },
+  { name: "Swae Lee", spotify: "https://open.spotify.com/artist/1zNqQNIdeOUZHb8zbZRFMX" },
+  { name: "Jhené Aiko", spotify: "https://open.spotify.com/artist/5ZS223C6JyBfXasXxrRqOk" },
+  { name: "Baby Storme", spotify: "https://open.spotify.com/artist/7cwYKm54nTTbnm0UnH0xBe" },
+];
+
+// Search function
+document.getElementById('searchButton').addEventListener('click', () => {
+  const input = document.getElementById('searchInput').value.trim().toLowerCase();
+  const artist = artists.find(a => a.name.toLowerCase() === input);
+  if (artist) {
+    window.open(artist.spotify, '_blank');
+  } else {
+    alert('Artist not found. Please check the spelling.');
+  }
+});
